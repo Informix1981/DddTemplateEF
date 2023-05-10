32b3e38cc02953af0845f7b5238c0b7528145a1d
Zied Kefi <Zied.KEFI@axefinance.com>
2023-05-10 20:24:19 +0100

    Add Echo

---
+++ WebApi/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("Echo")]
+        public ActionResult Echo()
+        {
+            return Ok("Hello");
+        }
+    }
+}
